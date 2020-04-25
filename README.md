e536b45e63e53cde9fcc22afc11db478d49c85fa
DysphoricUnicorn <taxcamp.de@gmail.com>
2020-04-26 00:37:23 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Every systemd.
+
+A bot that posts random dictionary words with a d appended to the end and systemd before them to mastodon.
+
+## Deployment
+
+Honestly, why would you want to deploy this? Whatever.. Here's how you'd do that..
+
+### TODO
---
+++ every_systemd.py
@@ -0,0 +1,55 @@
+from mastodon import Mastodon
+import sys
+import json
+import random
+
+
+class EverySystemd:
+    mastodon = None
+    debug_mode = None
+    word_max_tries = None
+
+    def __init__(self, debug_mode):
+        try:
+            settings = json.load(open('settings.json'))
+            self.mastodon = Mastodon(
+                access_token=settings['mastodon_access_token'],
+                api_base_url=settings['mastodon_url'],
+                debug_requests=debug_mode
+            )
+            self.word_max_tries = settings['random_word_max_tries']
+            self.debug_mode = debug_mode
+            self.word_list = json.load(open('dict.json'))
+        except IndexError:
+            print("[ERROR] settings.json was not found or didn't contain all needed values. Please check if it is "
+                  + "configured properly")
+            sys.exit(1)
+
+    def do_shitpost(self):
+        word = self.get_random_word()
+        self.mastodon.status_post(status="systemd-" + word + "d", spoiler_text="Bot generated tech shitpost",
+                                  language="En", visibility="unlisted")
+
+    def get_random_word(self):
+        random_word = 'None'
+        tries = 0
+        while random_word == 'None' or tries >= self.word_max_tries or random_word[len(random_word) - 1] == 'd':
+            random_word = self.word_list[random.randint(0, len(self.word_list) -1)]
+            tries += 1
+        if tries == self.word_max_tries:
+            print("[ERROR] Could not come up with a word within " + str(self.word_max_tries) + " attempts.")
+            sys.exit(1)
+        return random_word
+
+
+if __name__ == '__main__':
+    try:
+        debug_mode_input = bool(sys.argv[1])
+    except IndexError:
+        debug_mode_input = False
+    except ValueError:
+        print('Expecting attr1 to be of type bool. Something that could not be converted to bool given. \n'
+              + 'Running in debug mode for safety')
+        debug_mode_input = True
+    bot = EverySystemd(debug_mode_input)
+    bot.do_shitpost()
